a95d91410791d9f3eb452765b2d4bc8fa3281411
Yoe <dembouz719@gmail.com>
2024-12-03 08:48:16 +0100

    imported simulation data to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Baseload from './components/Baseload';
 import PricePerHour from './components/PricePerHour';
+import SimulationData from './components/SimulationData';
 
 
 function App() {
 
   return (
     <>
+      <SimulationData />
       <Baseload />
       <PricePerHour />
     </>
